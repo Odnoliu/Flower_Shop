47bcd098a9f8505c55cdaa8d3c3e38a70df1c74b
Hong-Phuc-Cao <hongphuccaowork@gmail.com>
2025-11-03 18:31:02 +0700

    Create UserModel, UserController

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'asus/backend',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e40c40af3b92d7a0b6805451035265db47003677',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'asus/backend' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e40c40af3b92d7a0b6805451035265db47003677',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
